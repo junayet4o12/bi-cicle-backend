5b7a1b60753f62cf6e34c5bf3e396f758b16619a
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-02-19 23:29:22 +0600

    create product api endpoint address has changed

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -4,7 +4,7 @@ import { ProductControllers } from './product.controller';
 
 const router = Router();
 router.get('/', ProductControllers.getAllProducts)
-router.post('/create-product', ProductControllers.createProduct)
+router.post('/', ProductControllers.createProduct)
 router.get('/:productId', ProductControllers.getSingleProduct)
 router.put('/:productId', ProductControllers.updateSingleProduct)
 router.delete('/:productId', ProductControllers.deleteSingleProduct)
