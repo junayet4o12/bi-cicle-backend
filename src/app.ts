ff33781bf32044f3b5e53477112308805d9bfa53
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-03-17 22:39:41 +0600

    readme has updated

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: ['http://localhost:5173'] }))
 app.use('/api', router)
 
 app.get('/', (req: Request, res: Response) => {
