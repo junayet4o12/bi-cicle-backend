ee8a8225522ed0b6c64da3e077ba037986bee01f
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-03-18 23:47:13 +0600

    cookies parser has added

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import cors from 'cors'
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import cookieParser from 'cookie-parser';
 const app: Application = express();
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors({ origin: ['http://localhost:5173'] }))
 app.use('/api', router)
 
