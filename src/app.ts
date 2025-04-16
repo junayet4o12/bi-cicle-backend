f8f65b4f5f1c9ecb3001166f868a17a3c7a6f5b0
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-04-16 12:10:09 +0600

    some endpoint has updated

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 const app: Application = express();
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true, }))
+app.use(cors({ origin: ['http://localhost:5173', 'https://cycle-craze-frontend.vercel.app'], credentials: true, }))
 app.use('/api', router)
 
 app.get('/', (req: Request, res: Response) => {
